d87c4a8f70f61a1278a6d07425fade5b4798b96e
Paul Barker <paul_barker@live.com>
2015-01-17 02:45:54 -0700

    Homepage and Bootstrap Upload

---
+++ assignments.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Homepage for CIT 313">
+    <meta name="author" content="Paul Barker">
+
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+  </head>
+
+  <body>
+   
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li><a href="index.php">Home</a></li>
+                  <li class="active"><a href="#">Assignments</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          
+          <div class='row text-center'>
+            <div class='col-sm-12'>
+              <h1>Coming Soon</h1>
+            </div>
+          </div>
+          
+          <div class="mastfoot">
+            <div class="inner">
+              <p>&copy;2014 Paul Barker</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Homepage for CIT 313">
+    <meta name="author" content="Paul Barker">
+
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="assignments.php">Assignments</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <div class='row'>
+              <div class='col-md-4'>
+                <img class='img-responsive' src="portrait.jpg" alt="Paul Barker"/>
+              </div>
+              <div class='col-md-8'>
+                <div class='rw-words'>
+                  <h1 class="cover-heading">Hello.</h1>
+                  <h1 class="cover-heading">Bonjour.</h1>
+                  <h1 class="cover-heading">Hola.</h1>
+                  <h1 class="cover-heading">Aloha.</h1>
+                  <h1 class="cover-heading">今日は.</h1>
+                  <h1 class="cover-heading">Привет.</h1>
+                </div>
+                <p class="lead">My name is Paul Barker. I like the internet. And web design stuffs. This is my website for CIT 313. I'm pretty stoked about it.</p>
+              </div>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>&copy;2014 Paul Barker</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
